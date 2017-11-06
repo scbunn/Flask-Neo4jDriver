5c4273ec379c8114a0f549a3652c4f1c5bced8c6
Stephen Bunn <scbunn@sbunn.org>
2017-11-05 16:29:03 -0800

    removed python 2.x support due to metaclass

--- setup.py
+++ setup.py
@@ -46,10 +46,9 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6'
     ],
     keywords='flask neo4j database graph',
-    python_requires='>=2.7',
+    python_requires='>=3',
 )
